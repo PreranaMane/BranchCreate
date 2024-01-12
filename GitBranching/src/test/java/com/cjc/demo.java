c591e7d2379b259c8f1f7f62592bb55dcf409465
PreranaMane <149044575+PreranaMane@users.noreply.github.com>
2024-01-12 10:32:57 +0530

    By git

--- GitBranching/src/test/java/com/cjc/demo.java
+++ GitBranching/src/test/java/com/cjc/demo.java
@@ -2,7 +2,12 @@ package com.cjc;
 
 public class demo
 {
-  public static void main(String[] args) {
-System.out.println("Git Branch");
+  public void m1()
+  {
+    System.out.println("M1_Method");
+  }
+  public static void main(String[] args) 
+  {
+     System.out.println("Git Branch");
   }
 }
